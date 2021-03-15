7418a268d566581770c5799b84ecc699aea93a98
Angelo Fallein <angelo@mitus.be>
2021-03-15 11:14:21 +0100

    klasse taak afgerond

--- src/model/taak.php
+++ src/model/taak.php
@@ -1,6 +1,5 @@
 <?php
 
-
 class Taak{
     public $id;
     public $titel;
@@ -18,11 +17,29 @@ class Taak{
     public function getAfgewerktTekst(){
         // is het attribuut afgewerkt 0 dan rerturn je de string “afgewerkt”.
         // zoniet return je de string “bezig”
+        if ($this->afgewerkt){
+            return "afgewerkt";
+        }else{
+            return "bezig";
+        }
     }
     public function getPrioriteitTekst(){
         // return de string “laag”, “medium” of “hoog” terug.
         // Afhankelijk van de waarde van het attribuut prioriteit.
         // (1=laag, 2=medium, 3=hoog)
+        switch ($this->prioriteit) {
+            case 1:
+                return "laag";
+                break;
+            case 2:
+                return "medium";
+                break;
+            case 3:
+                return "hoog";
+                break;
+            default:
+                return "niet gekend";
+        }
     }
 
 }
