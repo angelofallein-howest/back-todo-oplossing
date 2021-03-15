f8a3542709f102164552e40554b94c74a10b3f4f
Angelo Fallein <angelo@mitus.be>
2021-03-15 10:37:18 +0100

    cursus code toegevoegd

--- create-taak-verwerk.php
+++ create-taak-verwerk.php
@@ -1,5 +1,11 @@
 <?php
+require_once dirname(__FILE__) . "/src/helper/debug.php"; 
+require_once dirname(__FILE__) . "/src/repository/todorepository.php"; 
 
-    //????????????
+if (isset($_POST["submit"])){
+    
+} else {
+    echo "toevoegen mislukt";
+}
 
 ?>
--- update-taak-verwerk.php
+++ update-taak-verwerk.php
@@ -1,4 +1,14 @@
 <?php
+require_once dirname(__FILE__) . "/src/helper/debug.php"; 
+require_once dirname(__FILE__) . "/src/repository/todorepository.php"; 
+require_once dirname(__FILE__) . "/src/model/taak.php"; 
 
-    //????????????
+if (isset($_POST["submit"])){
+
+} else {
+
+    echo "updaten mislukt";
+
+}
+
 ?>
